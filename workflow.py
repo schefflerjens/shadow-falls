ec27b0f51a2756f45933b4fb2e3d421a548d69ab
Jens Scheffler <schefflerjens@gmail.com>
2024-06-21 17:59:04 -0700

    use the following beat to increase consistency between the beats.

--- workflow.py
+++ workflow.py
@@ -34,7 +34,8 @@ class Workflow:
         """Create a clean editor chat (without chat history)"""
         return self.___new_chat(Persona.EDITOR)
 
-    def __pick_best(self, prompt: CommonPrompts, drafts: list[str], **kwargs) -> str:
+    def __pick_best(self, prompt: CommonPrompts,
+                    drafts: list[str], **kwargs) -> str:
         assert len(drafts), 'Drafts array should never be empty!!!'
         if len(drafts) == 1:
             return drafts[0]
@@ -77,7 +78,12 @@ class Workflow:
             chapter=chapter
         )
 
-    def __write_beat(self, current_beat: str, prior_output: str, prior_beats: str, setting: str) -> str:
+    def __write_beat(self,
+                     current_beat: str,
+                     next_beat: str,
+                     prior_output: str,
+                     prior_beats: str,
+                     setting: str) -> str:
         """Write several drafts for the next beat and return the best one."""
         drafts = []
         logger.debug('Writing %s versions of the current beat: %s' % (
@@ -88,10 +94,12 @@ class Workflow:
                 previous_scene=prior_output,
                 start_with=last_sentences(prior_output, 2),
                 current_beat=current_beat,
+                next_beat=next_beat,
                 prior_beats=prior_beats,
                 setting=setting))
-            # Some prompts might instruct the LLM to quote the last few sentences of prior_output
-            # for consistency. Let's make sure that any such overlap is stripped away.
+            # Some prompts might instruct the LLM to quote the last few
+            # sentences of prior_output for consistency.
+            # Let's make sure that any such overlap is stripped away.
             draft = remove_overlap(prior_output.strip(), draft.strip()).strip()
             drafts.append(draft)
         # Pick the best draft
@@ -114,7 +122,8 @@ class Workflow:
         if self.__config.chapter_number() > 1:
             logger.debug('Fetching prior output from properties.yaml')
             prior_output = self.__config.get_property(
-                self.__config.chapter_number() - 1, PropertyKeys.CHAPTER_SNIPPET)
+                self.__config.chapter_number() - 1,
+                PropertyKeys.CHAPTER_SNIPPET)
             if not prior_output:
                 logger.fatal('Error, previous chapter sample not found. '
                              'Did you run the UPDATE_METADATA command?')
@@ -130,8 +139,11 @@ class Workflow:
         for count, current_beat in enumerate(story_beats):
             logger.info('Writing beat %s of %s' %
                         ((count + 1, len(story_beats))))
+            next_beat = '(END OF CHAPTER, consider ending with a cliffhanger)'
+            if count + 1 < len(story_beats):
+                next_beat = story_beats[count + 1]
             next_output = self.__write_beat(
-                current_beat, prior_output, prior_beats, settings)
+                current_beat, next_beat, prior_output, prior_beats, settings)
             output.append(next_output)
             prior_beats = self.write_summary('%s\n%s' % (
                 prior_beats,
